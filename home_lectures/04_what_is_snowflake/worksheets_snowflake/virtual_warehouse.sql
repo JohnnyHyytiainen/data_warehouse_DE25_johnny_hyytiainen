9bdc74c8b5d7f6176e5dd727d617b4211c7d1abb
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-09-08 14:36:47 +0200

    feat(virtual warehouse): created a warehouse demo, altered its compute, set its auto suspend, increased its max cluster count and dropped it

--- home_lectures/04_what_is_snowflake/worksheets_snowflake/virtual_warehouse.sql
+++ home_lectures/04_what_is_snowflake/worksheets_snowflake/virtual_warehouse.sql
@@ -1,28 +1,36 @@
+-- Showing warehouses
+-- Visar vilka warehouses du har
 SHOW WAREHOUSES;
 
-CREATE WAREHOUSE demo_warehouse
-WITH 
-WAREHOUSE_SIZE = 'X-Small' -- t-shirt sizes
-AUTO_SUSPEND = 300 
+-- Skapande av ett nytt warehouse
+-- Sätt STORLEKEN på skapade warehouset(VIKTIGT)
+-- Storlekarna är 'X-Small', 'Medium', 'Large', 'X-Large'... ...
+-- AUTO_SUSPEND = 300 innebär att den droppas efter 5 minuter
+-- AUTO_RESUME = TRUE innebär att den startar upp vid användning
+CREATE WAREHOUSE warehouse_demo
+WITH
+WAREHOUSE_SIZE = 'X-Small'
+AUTO_SUSPEND = 300
 AUTO_RESUME = TRUE
 INITIALLY_SUSPENDED = TRUE
-COMMENT = 'Demo warehouse created through worksheet';
-
-SHOW WAREHOUSES;
-
-USE WAREHOUSE COMPUTE_WH;
+COMMENT = 'Demo warehouse created through a worksheet';
 
 SHOW WAREHOUSES;
 
 
 -- DDL operation: ALTER to modify compute resources (warehouses) and database objects
+-- Jag kan alter(ändra) på mina resources och modifiera mina compute resources för att spara credits.
 ALTER WAREHOUSE COMPUTE_WH
-SET AUTO_SUSPEND = 60; -- to decrease cost 
+SET AUTO_SUSPEND = 60;
+
 
 -- scaling out - horizontal scaling
-ALTER WAREHOUSE DEMO_WAREHOUSE
+-- Om jag har flera mindre operations och vill skala med mindre operations som körs sekventiellt.
+ALTER WAREHOUSE WAREHOUSE_DEMO
 SET MAX_CLUSTER_COUNT = 3;
 
 SHOW WAREHOUSES;
 
-DROP WAREHOUSE DEMO_WAREHOUSE;
+DROP WAREHOUSE WAREHOUSE_DEMO;
+
+SHOW WAREHOUSES;
