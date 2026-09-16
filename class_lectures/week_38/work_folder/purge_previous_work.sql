d70712c6a8e334bfb2090342dc1144e00b530a00
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-09-16 09:45:14 +0200

    feat(cleanup): add clean-up script to remove roles, users + database that was created before implementation of home_ prefix for home lectures

---
+++ class_lectures/week_38/work_folder/purge_previous_work.sql
@@ -0,0 +1,29 @@
+--
+-- Purge script för att rensa tidigare data från home_lecture/ som skapades INNAN jag satte home_ prefixet för att särskilja på home lectures vs in class lectures
+-- 1. Välj rätt roll(SYSTEMADMIN), droppa databas, 
+-- 2. Välj rätt roll(USERADMIN) som äger skapade användarna, droppa skapade användare.
+-- 3. Dubbelkolla att ingenting ligger kvar och skräpar
+-- 4. Dubbelkolla alla roller
+-- 5. Dubbelkolla alla users
+--
+--
+-- 1. Databasen först, tabellerna försvinner med den
+USE ROLE SYSADMIN;
+DROP DATABASE IF EXISTS movies;
+
+-- 2. Roller och användare, USERADMIN skapade dem och äger dem
+USE ROLE USERADMIN;
+DROP ROLE IF EXISTS movies_dlt_role;
+DROP ROLE IF EXISTS movies_reader_role;
+DROP ROLE IF EXISTS movies_reader;
+DROP USER IF EXISTS extract_loader;
+
+-- 3. Kontrollera att inget ligger kvar
+SHOW ROLES LIKE 'movies%';
+SHOW USERS LIKE 'extract%';
+
+-- 4. Dubbelkolla rollerna
+SHOW ROLES;
+
+-- 5. Dubbelkolla användare
+SHOW USERS;
